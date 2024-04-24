aaa2cf8e64594f099de3b59ec4845641f260fae2
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-24 15:27:24 +0200

    Added embedding (will be handy for assets)

--- utils/tooling/embedder.py
+++ utils/tooling/embedder.py
@@ -17,16 +17,19 @@ def embedder(input_file, output_file):
         f.write('#pragma once\n\n')
         f.write('const char* ' + name + ' = ' + file_as_raw_string + ';\n')
 
-
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python embedder.py <input file> <output.hpp>")
-        return
-
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    embedder(input_file, output_file)
-
-if __name__ == "__main__":
-    main()
+def embedder_bulk(input_dir, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    for input_root, directories, files in os.walk(input_dir):
+        output_root = input_root.replace(input_dir, output_dir)
+        for directory in directories:
+            input_directory = os.path.join(input_root, directory)
+            output_directory = os.path.join(output_root, directory)
+            if not os.path.exists(output_directory):
+                os.makedirs(output_directory)
+        for file in files:
+            if not os.path.exists(output_root):
+                os.makedirs(output_root)
+            input_file = os.path.join(input_root, file)
+            output_file = os.path.join(output_root, file + '.hpp')
+            embedder(input_file, output_file)
