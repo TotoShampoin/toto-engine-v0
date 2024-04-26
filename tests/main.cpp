8d46ea5fcbc53cbc0a2c30357e561537f646c7e7
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-26 14:27:15 +0200

    Moving things around

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,7 @@
 #include <TotoEngine/TotoEngine.hpp>
 
+#include <algorithm>
+#include <chrono>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
@@ -10,9 +12,11 @@
 #include <vector>
 
 #include "TotoEngine/Graphics/ShaderProgram.hpp"
+#include "TotoEngine/Graphics/Texture.hpp"
+#include "TotoEngine/Transform.hpp"
 
 void imguiInit(TotoEngine::Window& window);
-void imguiRender();
+void imguiRender(float render_time, float delta_time);
 
 using GLTexture = TotoEngine::GLObject<
     [] { GLuint id; glGenTextures(1, &id); return id; },
@@ -27,22 +31,8 @@ int main(int /* argc */, const char* /* argv */[]) {
 
     imguiInit(window);
 
-    Texture2D texture;
-    Texture2D::bind(texture);
-    Texture2D::parameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    Texture2D::parameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    Texture2D::parameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    Texture2D::parameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    constexpr int width = 16, height = 16, channels = 4;
-    std::vector<uint8_t> data(width * height * channels, 0);
-    for(int i = 0; i < width * height; i++) {
-        data[i * channels + 0] = (i % 2) * 255;
-        data[i * channels + 1] = (i % 3) * 127;
-        data[i * channels + 2] = (i % 4) * 85;
-        data[i * channels + 3] = 255;
-    }
-    Texture2D::image(width, height, channels, data);
-    Texture2D::bind(Texture2D::NONE);
+    auto camera_projection = glm::perspective(glm::radians(70.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    auto camera_transform = Transform();
 
     auto vertex_buffer = GeometryBuffer(
         {
@@ -58,37 +48,62 @@ int main(int /* argc */, const char* /* argv */[]) {
     auto material = BasicMaterial();
     auto transform = Transform();
 
+    // TODO: Add a way to instantiate
+    material.map = Texture2D(); {
+        Texture2D::bind(material.map.value());
+        Texture2D::parameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        Texture2D::parameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        Texture2D::parameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        Texture2D::parameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        constexpr int width = 16, height = 16, channels = 4;
+        std::vector<uint8_t> data(width * height * channels, 0);
+        for(int i = 0; i < width * height; i++) {
+            data[i * channels + 0] = (i % 2) * 255;
+            data[i * channels + 1] = (i % 3) * 127;
+            data[i * channels + 2] = (i % 4) * 85;
+            data[i * channels + 3] = 255;
+        }
+        Texture2D::image(width, height, channels, data);
+        Texture2D::bind(Texture2D::NONE);
+    }
+
     transform.translate({0.0f, 0.0f, -2.0f});
 
-    auto projection = glm::perspective(glm::radians(70.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-    auto view = Matrix4(1.0f);
-
-    ShaderProgram::use(material.shader());
-    material.shader().uniform("u_projection", projection);
-    material.shader().uniform("u_view", view);
-    material.shader().uniform("u_model", transform.matrix());
-    material.map = std::move(texture);
+    auto start_time = std::chrono::high_resolution_clock::now();
+    auto last_time = start_time;
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     while(!window.shouldClose()) {
+        auto current_time = std::chrono::high_resolution_clock::now();
+        auto time = std::chrono::duration<float>(current_time - start_time).count();
+        auto delta_time = std::chrono::duration<float>(current_time - last_time).count();
+
         Window::makeContextCurrent(window);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         GeometryBuffer::bind(vertex_buffer);
         ShaderProgram::use(material.shader());
+        material.shader().uniform("u_projection", camera_projection);
+        material.shader().uniform("u_view", glm::inverse(camera_transform.matrix()));
         material.shader().uniform("u_model", transform.matrix());
         material.apply();
         glDrawElements(GL_TRIANGLES, vertex_buffer.indices().size(), GL_UNSIGNED_INT, nullptr);
 
-        imguiRender();
+        auto time_after_render = std::chrono::high_resolution_clock::now();
+        auto render_time = std::chrono::duration<float>(time_after_render - current_time).count();
+
+        imguiRender(render_time, delta_time);
 
         Window::swapBuffers(window);
         Window::pollEvents();
 
         transform.rotate(glm::radians(1.0f), {0.0f, 1.0f, 0.0f});
+        camera_transform.position().x = glm::cos(time);
+
+        last_time = current_time;
     }
 
     return 0;
@@ -101,15 +116,22 @@ void imguiInit(TotoEngine::Window& window) {
     ImGui_ImplGlfw_InitForOpenGL(window.GLFWWindow(), true);
     ImGui_ImplOpenGL3_Init("#version 460");
 }
-void imguiRender() {
+void imguiRender(float render_time, float delta_time) {
+    static std::vector<float> render_times;
+    render_times.push_back(1.f / render_time);
+    if(render_times.size() > 200) {
+        render_times.erase(render_times.begin());
+    }
+
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
     ImGui::SetNextWindowPos(ImVec2(0, 0));
     ImGui::SetNextWindowSize(ImVec2(0, 0));
-    ImGui::Begin("FPS", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-    ImGui::Text("%3.2f", ImGui::GetIO().Framerate);
+    ImGui::Begin("Timing", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoBringToFrontOnFocus);
+    ImGui::PlotHistogram("##fps", render_times.data(), render_times.size(), 0, nullptr, 0, std::max_element(render_times.begin(), render_times.end()).operator*(), ImVec2(400, 80));
+    ImGui::Text("Render FPS: %.2f", 1.f / render_time);
     ImGui::End();
 
     ImGui::Render();
