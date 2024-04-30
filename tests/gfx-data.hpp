323e07b5aa725ce961d78261dce11400fe66dc31
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-04-30 21:33:30 +0200

    Refactor for shorter main

---
+++ tests/gfx-data.hpp
@@ -0,0 +1,150 @@
+#pragma once
+
+#include "TotoEngine/Graphics/GPUObjects/GeometryBuffer.hpp"
+#include "TotoEngine/Graphics/Loaders/TextureLoader.hpp"
+#include "TotoEngine/Graphics/RenderData/Camera.hpp"
+#include "TotoEngine/Graphics/RenderData/Light.hpp"
+#include "TotoEngine/Graphics/RenderData/Materials.hpp"
+#include "TotoEngine/Transform.hpp"
+#include "TotoEngine/Window.hpp"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <TotoEngine/Graphics/GPUObjects/Texture.hpp>
+#include <TotoEngine/Graphics/GPUObjects/ShaderProgram.hpp>
+#include <TotoEngine/TotoEngine.hpp>
+#include <functional>
+#include <vector>
+
+struct DeferredRendering {
+    DeferredRendering(int width, int height):
+        buffer(width, height, {
+            TotoEngine::TextureFormat::RGB32F, // Position
+            TotoEngine::TextureFormat::RGB32F, // Normal
+            TotoEngine::TextureFormat::RGB32F, // Ambient
+            TotoEngine::TextureFormat::RGB32F, // Diffuse
+            TotoEngine::TextureFormat::RGB32F, // Specular
+            TotoEngine::TextureFormat::RGB32F, // Emissive
+            TotoEngine::TextureFormat::RGB32F, // Shininess
+            TotoEngine::TextureFormat::RGB32F, // Alpha
+        }),
+        shader(
+            TotoEngine::loadShaderFile<TotoEngine::ShaderType::VERTEX>("tests_assets/screen.vert"),
+            TotoEngine::loadShaderFile<TotoEngine::ShaderType::FRAGMENT>("tests_assets/deferred.frag")
+        ),
+        hdri(TotoEngine::loadTexture2D("tests_assets/hdri.jpg"))
+    {}
+
+    void apply() {
+        TotoEngine::Texture2D::bindAs(hdri, 0);
+        TotoEngine::Texture2D::bindAs(buffer.texture(POSITION), POSITION + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(NORMAL), NORMAL + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(AMBIENT), AMBIENT + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(DIFFUSE), DIFFUSE + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(SPECULAR), SPECULAR + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(EMISSIVE), EMISSIVE + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(SHININESS), SHININESS + 1);
+        TotoEngine::Texture2D::bindAs(buffer.texture(ALPHA), ALPHA + 1);
+        shader.uniform("u_hdri", 0);
+        shader.uniform("u_position", POSITION + 1);
+        shader.uniform("u_normal", NORMAL + 1);
+        shader.uniform("u_ambient", AMBIENT + 1);
+        shader.uniform("u_diffuse", DIFFUSE + 1);
+        shader.uniform("u_specular", SPECULAR + 1);
+        shader.uniform("u_emissive", EMISSIVE + 1);
+        shader.uniform("u_shininess", SHININESS + 1);
+        shader.uniform("u_alpha", ALPHA + 1);
+    }        
+
+    TotoEngine::FrameBuffer buffer;
+    TotoEngine::ShaderProgram shader;
+    TotoEngine::Texture2D hdri;
+
+    static constexpr auto POSITION = 0;
+    static constexpr auto NORMAL = 1;
+    static constexpr auto AMBIENT = 2;
+    static constexpr auto DIFFUSE = 3;
+    static constexpr auto SPECULAR = 4;
+    static constexpr auto EMISSIVE = 5;
+    static constexpr auto SHININESS = 6;
+    static constexpr auto ALPHA = 7;
+};
+
+struct MyMaterial {
+    MyMaterial():
+        uv_texture(TotoEngine::loadTexture2D("tests_assets/uv.png")),
+        shader(
+            TotoEngine::loadShaderFile<TotoEngine::ShaderType::VERTEX>("tests_assets/basic.vert"),
+            TotoEngine::loadShaderFile<TotoEngine::ShaderType::FRAGMENT>("tests_assets/phong_pass.frag")
+        )
+    {
+        material.diffuse_map = uv_texture;
+        material.specular = TotoEngine::ColorRGB(1.0f);
+        material.shininess = 64.f;
+        material.ambient_map = uv_texture;
+    }
+
+    void apply() {
+        material.apply(shader);
+    }
+
+    TotoEngine::Texture2D uv_texture;
+    TotoEngine::ShaderProgram shader;
+    TotoEngine::PhongMaterial material;
+};
+
+struct Mesh {
+    TotoEngine::GeometryBuffer model;
+    TotoEngine::Transform transform {};
+};
+
+inline void renderDeferred(
+    TotoEngine::Window& window,
+    DeferredRendering& deferred, MyMaterial& material,
+    const std::vector<std::reference_wrapper<Mesh>>& meshes,
+    const std::vector<TotoEngine::Light>& lights,
+    TotoEngine::Camera& camera,
+    Mesh& screen
+) {
+    TotoEngine::Renderer::bindRenderTarget(deferred.buffer);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    TotoEngine::ShaderProgram::use(material.shader);
+    material.apply();
+    for(const Mesh& mesh : meshes) {
+        TotoEngine::GeometryBuffer::bind(mesh.model);
+        TotoEngine::Renderer::apply(material.shader, camera, mesh.transform);
+        TotoEngine::Renderer::draw(mesh.model);
+    }
+
+    TotoEngine::Renderer::bindRenderTarget(window);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    TotoEngine::GeometryBuffer::bind(screen.model);
+    TotoEngine::ShaderProgram::use(deferred.shader);
+    deferred.apply();
+    TotoEngine::Renderer::apply(deferred.shader, camera, screen.transform);
+    TotoEngine::Renderer::apply(deferred.shader, camera, lights);
+    TotoEngine::Renderer::draw(screen.model);
+}
+
+inline void imguiRender(
+    float& render_time
+) {
+    static std::vector<float> render_times;
+    render_times.push_back(1.f / render_time);
+    if(render_times.size() > 200) {
+        render_times.erase(render_times.begin());
+    }
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::SetNextWindowSize(ImVec2(0, 0));
+    ImGui::Begin("Timing", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoBringToFrontOnFocus);
+    ImGui::PlotHistogram("##fps", render_times.data(), render_times.size(), 0, nullptr, 0, std::max_element(render_times.begin(), render_times.end()).operator*(), ImVec2(400, 80));
+    ImGui::Text("Render FPS: %.2f", 1 / render_time);
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,3 +1,4 @@
+#include "gfx-data.hpp"
 #include <TotoEngine/TotoEngine.hpp>
 
 #include <imgui.h>
@@ -9,7 +10,6 @@
 #include <glm/trigonometric.hpp>
 
 #include <vector>
-#include <algorithm>
 #include <chrono>
 
 int main(int /* argc */, const char* /* argv */[]) {
@@ -24,96 +24,26 @@ int main(int /* argc */, const char* /* argv */[]) {
     ImGui_ImplGlfw_InitForOpenGL(window.GLFWWindow(), true);
     ImGui_ImplOpenGL3_Init("#version 460");
 
-    auto deferred_buffer = FrameBuffer(800, 600, {
-        TextureFormat::RGB32F, // Position
-        TextureFormat::RGB32F, // Normal
-        TextureFormat::RGB32F, // Ambient
-        TextureFormat::RGB32F, // Diffuse
-        TextureFormat::RGB32F, // Specular
-        TextureFormat::RGB32F, // Emissive
-        TextureFormat::RGB32F, // Shininess
-        TextureFormat::RGB32F, // Alpha
-    });
-    constexpr auto POSITION = 0;
-    constexpr auto NORMAL = 1;
-    constexpr auto AMBIENT = 2;
-    constexpr auto DIFFUSE = 3;
-    constexpr auto SPECULAR = 4;
-    constexpr auto EMISSIVE = 5;
-    constexpr auto SHININESS = 6;
-    constexpr auto ALPHA = 7;
-
-    auto deferred_shader = ShaderProgram(
-        loadShaderFile<VERTEX>("tests_assets/screen.vert"),
-        loadShaderFile<FRAGMENT>("tests_assets/deferred.frag")
-    );
-    auto hdri_texture = loadTexture2D("tests_assets/hdri.jpg");
-    auto apply_deferred = [&deferred_buffer, &hdri_texture](ShaderProgram& deferred_shader) {
-        Texture2D::bindAs(hdri_texture, 0);
-        Texture2D::bindAs(deferred_buffer.texture(POSITION), POSITION + 1);
-        Texture2D::bindAs(deferred_buffer.texture(NORMAL), NORMAL + 1);
-        Texture2D::bindAs(deferred_buffer.texture(AMBIENT), AMBIENT + 1);
-        Texture2D::bindAs(deferred_buffer.texture(DIFFUSE), DIFFUSE + 1);
-        Texture2D::bindAs(deferred_buffer.texture(SPECULAR), SPECULAR + 1);
-        Texture2D::bindAs(deferred_buffer.texture(EMISSIVE), EMISSIVE + 1);
-        Texture2D::bindAs(deferred_buffer.texture(SHININESS), SHININESS + 1);
-        Texture2D::bindAs(deferred_buffer.texture(ALPHA), ALPHA + 1);
-        deferred_shader.uniform("u_hdri", 0);
-        deferred_shader.uniform("u_position", POSITION + 1);
-        deferred_shader.uniform("u_normal", NORMAL + 1);
-        deferred_shader.uniform("u_ambient", AMBIENT + 1);
-        deferred_shader.uniform("u_diffuse", DIFFUSE + 1);
-        deferred_shader.uniform("u_specular", SPECULAR + 1);
-        deferred_shader.uniform("u_emissive", EMISSIVE + 1);
-        deferred_shader.uniform("u_shininess", SHININESS + 1);
-        deferred_shader.uniform("u_alpha", ALPHA + 1);
-    };
-
     auto camera = Camera(glm::perspective(glm::radians(70.0f), 320.0f / 240.0f, 0.1f, 100.0f));
-
-    auto uv_texture = loadTexture2D("tests_assets/uv.png");
-
-    auto shader = ShaderProgram(
-        loadShaderFile<VERTEX>("tests_assets/basic.vert"),
-        loadShaderFile<FRAGMENT>("tests_assets/phong_pass.frag")
-    );
-    auto material = PhongMaterial();
-        material.diffuse_map = uv_texture;
-        material.specular = ColorRGB(1.0f);
-        material.shininess = 64.f;
-        material.ambient_map = uv_texture;
-    ShaderProgram::use(shader);
-    auto apply_material = [&material](ShaderProgram& shader) {
-        material.apply(shader);
-    };
-
-    auto screen_geometry = plane(2, 2);
-    auto screen_transform = Transform();
-        screen_transform.position() = {0, 0, -1};
-
-    auto plane_model = cube(2, 2, 2);
-    auto plane_transform = Transform();
-        plane_transform.position() = {0, -2, -5};
-
-    auto sphere_model = sphere(1, 32, 16);
-    auto sphere_transform = Transform();
-        sphere_transform.translate({0.0f, 0.0f, -5.0f});
-
-    auto amb_light = Light(LightType::AMBIENT, ColorRGB(1.0f, 1.0f, 1.0f), 0.33333f);
-    auto dir_light = Light(LightType::DIRECTIONAL, ColorRGB(1.0f, 1.0f, 1.0f), 1.0f);
-        dir_light.position() = {0, 0, 0};
-    auto pt_light = Light(LightType::POINT, ColorRGB(1.0f, 1.0f, 1.0f), 2.0f);
-        pt_light.position() = {0, 2, -4};
-
-    glm::vec3 dir_light_target = {0, 1, 1};
-
-    auto light_helper_transform = Transform();
-        light_helper_transform.scale() *= .1;
+    auto deferred = DeferredRendering(800, 600);
+    auto material = MyMaterial();
+    auto screen_model = Mesh { plane(2, 2) };
+    auto plane_model = Mesh { cube(2, 2, 2) };
+    auto sphere_model = Mesh { sphere(1, 32, 16)  };
+    auto amb_light = Light(LightType::AMBIENT, ColorRGB(1, 1, 1), 0.33333f);
+    auto dir_light = Light(LightType::DIRECTIONAL, ColorRGB(1, 1, 1), 1);
+    auto pt_light = Light(LightType::POINT, ColorRGB(1, 1, 1), 2);
+    screen_model.transform.position() = {0, 0, -1};
+    plane_model.transform.position() = {0, -2, -5};
+    sphere_model.transform.position() = {0, 0, -5};
+    dir_light.position() = {0, 0, 0};
+    dir_light.lookAt({0, 1, 1});
+    pt_light.position() = {0, 2, -4};
 
     auto start_time = std::chrono::high_resolution_clock::now();
     auto last_time = start_time;
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0, 0, 0, 0);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -128,70 +58,24 @@ int main(int /* argc */, const char* /* argv */[]) {
 
         Window::makeContextCurrent(window);
 
-        { // Deferred shading
-            Renderer::bindRenderTarget(deferred_buffer);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            ShaderProgram::use(shader);
-            Renderer::apply(shader, apply_material);
-            GeometryBuffer::bind(plane_model);
-            Renderer::apply(shader, camera, plane_transform);
-            Renderer::draw(plane_model);
-            GeometryBuffer::bind(sphere_model);
-            Renderer::apply(shader, camera, sphere_transform);
-            Renderer::draw(sphere_model);
-
-            Renderer::bindRenderTarget(window);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            ShaderProgram::use(deferred_shader);
-            GeometryBuffer::bind(screen_geometry);
-            Renderer::apply(deferred_shader, apply_deferred);
-            Renderer::apply(deferred_shader, camera, screen_transform);
-            Renderer::apply(deferred_shader, camera, {dir_light, pt_light});
-            Renderer::draw(screen_geometry);
-        }
+        renderDeferred(
+            window, deferred, material,
+            { plane_model, sphere_model }, { dir_light , pt_light },
+            camera, screen_model
+        );
         
         auto time_after_render = std::chrono::high_resolution_clock::now();
         auto render_time = std::chrono::duration<float>(time_after_render - current_time).count();
 
-        {// imgui
-            static std::vector<float> render_times;
-            render_times.push_back(1.f / render_time);
-            if(render_times.size() > 200) {
-                render_times.erase(render_times.begin());
-            }
-
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            ImGui::SetNextWindowPos(ImVec2(0, 0));
-            ImGui::SetNextWindowSize(ImVec2(0, 0));
-            ImGui::Begin("Timing", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoBringToFrontOnFocus);
-            ImGui::PlotHistogram("##fps", render_times.data(), render_times.size(), 0, nullptr, 0, std::max_element(render_times.begin(), render_times.end()).operator*(), ImVec2(400, 80));
-            ImGui::Text("Render FPS: %.2f", 1.f / render_time);
-            ImGui::End();
-
-            ImGui::SetNextWindowPos(ImVec2(width, 0), ImGuiCond_Always, ImVec2(1, 0));
-            ImGui::SetNextWindowSize(ImVec2(0, 0));
-            ImGui::Begin("Light", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoBringToFrontOnFocus);
-            ImGui::SliderFloat3("Dir light Target", &dir_light_target.x, -2, 2);
-            ImGui::SliderFloat3("Pt light Position", &pt_light.position().x, -10, 10);
-            ImGui::SliderFloat3("Camera Position", &camera.position().x, -10, 10);
-            ImGui::End();
-
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        }
+        imguiRender(render_time);
 
         Window::swapBuffers(window);
         Window::pollEvents();
 
-        plane_transform.rotation() = glm::vec3(time);
-        sphere_transform.position().x = glm::sin(time);
-        sphere_transform.rotation().y = time;
+        plane_model.transform.rotation() = glm::vec3(time);
+        sphere_model.transform.position().x = glm::sin(time);
+        sphere_model.transform.rotation().y = time;
         camera.lookAt({0, 0, -5});
-        dir_light.lookAt(dir_light_target);
-        light_helper_transform.position() = pt_light.position();
 
         last_time = current_time;
     }
