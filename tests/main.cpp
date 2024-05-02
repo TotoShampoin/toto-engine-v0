bfb10e66f7dbef6544c7ad98cb2236f9f0d6aa5d
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-05-02 13:40:07 +0200

    Y

--- tests/main.cpp
+++ tests/main.cpp
@@ -16,32 +16,33 @@
 #include <chrono>
 
 int main(int /* argc */, const char* /* argv */[]) {
-    using namespace TotoEngine;
-    using namespace TotoEngine::Graphics;
-    using namespace TotoEngine::Audio;
-    using TextureTarget::TEXTURE_2D;
-    using ShaderType::VERTEX;
-    using ShaderType::FRAGMENT;
-    auto window = Window(800, 600, "TotoEngine");
+    using TotoEngine::Graphics::TextureTarget::TEXTURE_2D;
+    using TotoEngine::Graphics::ShaderType::VERTEX;
+    using TotoEngine::Graphics::ShaderType::FRAGMENT;
+    using TotoEngine::Graphics::LightType::AMBIENT;
+    using TotoEngine::Graphics::LightType::DIRECTIONAL;
+    using TotoEngine::Graphics::LightType::POINT;
+    
+    auto window = TotoEngine::Window(800, 600, "TotoEngine");
     glewInit();
 
-    auto& device = DeviceManager::open();
-    (void)Context(device);
+    auto& device = TotoEngine::Audio::DeviceManager::open();
+    (void)TotoEngine::Audio::Context(device);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGui_ImplGlfw_InitForOpenGL(window.GLFWWindow(), true);
     ImGui_ImplOpenGL3_Init("#version 460");
 
-    auto camera = Camera(glm::perspective(glm::radians(70.0f), 320.0f / 240.0f, 0.1f, 100.0f));
+    auto camera = TotoEngine::Graphics::Camera(glm::perspective(glm::radians(70.0f), 320.0f / 240.0f, 0.1f, 100.0f));
     auto deferred = DeferredRendering(800, 600);
     auto material = MyMaterial();
-    auto screen_model = Mesh { plane(2, 2) };
-    auto plane_model = Mesh { cube(2, 2, 2) };
-    auto sphere_model = Mesh { sphere(1, 32, 16)  };
-    auto amb_light = Light(LightType::AMBIENT, ColorRGB(1, 1, 1), 0.33333f);
-    auto dir_light = Light(LightType::DIRECTIONAL, ColorRGB(1, 1, 1), 1);
-    auto pt_light = Light(LightType::POINT, ColorRGB(1, 1, 1), 2);
+    auto screen_model = Mesh { TotoEngine::Graphics::plane(2, 2) };
+    auto plane_model = Mesh { TotoEngine::Graphics::cube(2, 2, 2) };
+    auto sphere_model = Mesh { TotoEngine::Graphics::sphere(1, 32, 16)  };
+    auto amb_light = TotoEngine::Graphics::Light(AMBIENT, TotoEngine::ColorRGB(1, 1, 1), 0.33333f);
+    auto dir_light = TotoEngine::Graphics::Light(DIRECTIONAL, TotoEngine::ColorRGB(1, 1, 1), 1);
+    auto pt_light = TotoEngine::Graphics::Light(POINT, TotoEngine::ColorRGB(1, 1, 1), 2);
     screen_model.transform.position() = {0, 0, -1};
     plane_model.transform.position() = {0, -2, -5};
     sphere_model.transform.position() = {0, 0, -5};
@@ -49,7 +50,7 @@ int main(int /* argc */, const char* /* argv */[]) {
     dir_light.lookAt({0, 1, 1});
     pt_light.position() = {0, 2, -4};
 
-    auto sample = loadSample("tests_assets/Bass-Drum-1.wav");
+    auto sample = TotoEngine::Audio::loadSample("tests_assets/Bass-Drum-1.wav");
 
     auto start_time = std::chrono::high_resolution_clock::now();
     auto last_time = start_time;
@@ -68,7 +69,7 @@ int main(int /* argc */, const char* /* argv */[]) {
         auto [width, height] = window.size();
         camera.projectionMatrix() = glm::perspective(glm::radians(70.0f), (float)width / height, 0.1f, 100.0f);
 
-        Window::makeContextCurrent(window);
+        TotoEngine::Window::makeContextCurrent(window);
 
         renderDeferred(
             window, deferred, material,
@@ -81,8 +82,8 @@ int main(int /* argc */, const char* /* argv */[]) {
 
         imguiRender(render_time, plays, sample);
 
-        Window::swapBuffers(window);
-        Window::pollEvents();
+        TotoEngine::Window::swapBuffers(window);
+        TotoEngine::Window::pollEvents();
 
         plane_model.transform.rotation() = glm::vec3(time);
         sphere_model.transform.position().x = glm::sin(time);
